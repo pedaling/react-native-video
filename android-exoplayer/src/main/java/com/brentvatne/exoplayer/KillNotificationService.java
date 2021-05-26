53d00e278ac4d86524c2ec88faa6ef51b4014f1c
Noel <noel@101.inc>
2021-05-26 16:47:10 +0900

    Revert "Merge pull request #13 from pedaling/fix/handle-intent-null"
    This reverts commit 9d50e12ea1aadfda57dc358f948087621e5bbabf, reversing
    changes made to e8de73babf77ccafcb49d9804062036be5da445e.

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/KillNotificationService.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/KillNotificationService.java
@@ -13,9 +13,6 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 public class KillNotificationService extends Service {
-  public static final String CHANNEL_ID = "@class101/kill_player_controller";
-  public static final String DEFAULT_CHANNEL_NAME = "클래스101 영상 컨트롤러";
-
   @Nullable
   @Override
   public IBinder onBind(Intent intent) {
@@ -25,17 +22,16 @@ public class KillNotificationService extends Service {
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      final String channelName = intent != null ?
-        intent.getStringExtra("channelName") : DEFAULT_CHANNEL_NAME;
+      final String channelName = intent.getStringExtra("channelName");
 
       NotificationManager notificationManager =
-        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
       NotificationChannel channel =
-        notificationManager.getNotificationChannel(CHANNEL_ID);
+        notificationManager.getNotificationChannel(ExoPlayerNotificationManager.CHANNEL_ID);
 
       if (channel == null) {
         channel = new NotificationChannel(
-          CHANNEL_ID,
+          ExoPlayerNotificationManager.CHANNEL_ID,
           channelName,
           NotificationManager.IMPORTANCE_LOW
         );
@@ -43,7 +39,7 @@ public class KillNotificationService extends Service {
       }
 
       Notification notification =
-        new NotificationCompat.Builder(this, CHANNEL_ID).build();
+        new NotificationCompat.Builder(this, ExoPlayerNotificationManager.CHANNEL_ID).build();
       startForeground(1, notification);
     }
 
@@ -54,24 +50,16 @@ public class KillNotificationService extends Service {
   public void onDestroy() {
     super.onDestroy();
 
-    killNotification();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      stopForeground(true);
+    }
   }
 
   @Override
   public void onTaskRemoved(Intent rootIntent) {
-    super.onTaskRemoved(rootIntent);
-
-    killNotification();
-  }
-
-  private void killNotification() {
     NotificationManager notificationManager =
       (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
     notificationManager.cancel(ExoPlayerNotificationManager.NOTIFICATION_ID);
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      stopForeground(true);
-    }
   }
 }
